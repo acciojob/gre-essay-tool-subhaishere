1907993afa7f756af4fcc96703b4f79ac0289633
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 12:42:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your code here
-const textbox = document.getElementById("evaluatedText");
-const wordCount = document.getElementById("wordCount");
+let text = document.getElementById('evaluatedText');
+let counter = document.getElementById('wordCount');
 
-textbox.addEventListener("input", function() {
-    let text = this.value.trim();
-    text = text.replace(/[^a-zA-Z0-9\s]/g, ""); // remove non-alphanumeric characters
-    wordCount.innerHTML = text.split(/\s+/).length;
+text.addEventListener('input', () => {
+  let textLength = text.value;
+  let count = textLength.split(" ").length;
+  counter.textContent = count;
 });
